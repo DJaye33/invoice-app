65521872f7592c938312ec3ec76d0dadfd7a5bcf
Danny JeanLouis <dlouis3386@gmail.com>
2023-07-06 00:09:04 -0400

    add logo

--- src/Components/AppBar/AppBar.tsx
+++ src/Components/AppBar/AppBar.tsx
@@ -1,8 +1,11 @@
 const AppBar = () => {
   return (
     <header className="flex bg-indigo-950">
-      <div className="bg-violet-600">
-        <img src={"/logo.svg"} alt="invoice app" />
+      <div className="bg-violet-600 w-[72px] h-[72px] relative flex justify-center items-center rounded-tr-3xl rounded-br-3xl">
+        <figure className="z-10">
+          <img src={"/logo.svg"} alt="invoice app" />
+        </figure>
+        <div className="bg-violet-500 h-3/6 absolute bottom-0 w-full rounded-tl-3xl rounded-br-3xl"></div>
       </div>
     </header>
   );
